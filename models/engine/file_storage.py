7ee9fd8b67b4ccf233da04486bcec4e726f1d6b4
abdelhadia72 <thisisavnb@gmail.com>
2023-11-08 00:32:21 +0100

    fix the realod method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 
 import json
 import uuid
+# from models.base_model import BaseModel
 from datetime import datetime
 
 class FileStorage():
@@ -23,27 +24,60 @@ class FileStorage():
     
     def save(self):
         json_object = {}
+        
         for key in FileStorage.__objects:
             # print("The key is: ",key)
             # print("info", FileStorage.__objects[key].to_dict())
             json_object[key] = FileStorage.__objects[key].to_dict()
+            # att
+
         
         with open(FileStorage.__file_path, 'w') as f:
-            json.dump(json_object, f, indent=4)
+            json.dump(json_object, f)
+    
+    # def reload(self):
+    #     print("Okay I'm running -00")
+    #     """Deserialize the JSON file __file_path to __objects, if it exists."""
+    #     try:
+    #         print("Okay I'm running 00")
+    #         with open(FileStorage.__file_path, 'r') as f:
+    #             dict_obj = json.load(f)
+    #             # load data to dicts
+    #             for value in dict_obj.values():
+    #                 # value is {}
+    #                 print("Okay I'm running 01") 
+    #                 print("VALUE IS IS IS ",value)
+    #                 class_name = value["__class__"]
+    #                 # print("I'M THAT CLASS ",class_name)
+    #                 del value["__class__"]  
+    #                 # print("my type is : ", type(eval(class_name(**value))))
+    #                 print("my type is : ", type(value))
+    #                 # FileStorage.new(eval(class_name(**value)))
+    #                 print("Okay I'm running 02")
+    #                 # print("EV_CLASS_NAME",eval(class_name)(**value))
+    #     except FileNotFoundError:
+    #         return
     
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(FileStorage.__file_path) as f:
+            with open(FileStorage.__file_path, 'r') as f:
                 dict_obj = json.load(f)
-                for value in dict_obj.values():
-                    # print("VALUE IS IS IS ",value)
+                for key, value in dict_obj.items():
                     class_name = value["__class__"]
-                    del value["__class__"]  
-                    self.new(eval(class_name)(**value))
-                    # print("EV_CLASS_NAME",eval(class_name)(**value))
+                    if class_name in globals():
+                        obj_class = globals()[class_name]
+                        del value["__class__"]
+                        obj = obj_class(**value)
+                        FileStorage.__objects[key] = obj
+                    else:
+                        from models.base_model import BaseModel
+                        obj = BaseModel(**value)
+                        FileStorage.__objects[key] = obj
         except FileNotFoundError:
             return
+
+
+
     
 # # #! remove me 
 # class Home():
