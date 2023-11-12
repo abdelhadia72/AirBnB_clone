1ab8b165dcaf1c1d93968277d23440bb89728863
abdelhadia72 <thisisavnb@gmail.com>
2023-11-12 16:09:44 +0100

    on test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,7 +5,6 @@ import unittest
 from console import HBNBCommand
 from unittest.mock import patch
 from io import StringIO
-from models import storage
 
 
 class TestHBNBCommand(unittest.TestCase):
@@ -48,54 +47,11 @@ class TestHBNBCommand(unittest.TestCase):
             self.cli.do_destroy("")
             self.assertEqual("** class name missing **\n", f.getvalue())
 
-    def test_do_all(self):
-        """Test all command"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.cli.do_all("")
-            self.assertEqual(str(storage.all()) + "\n", f.getvalue())
-
     def test_do_update(self):
         """Test update command"""
         with patch('sys.stdout', new=StringIO()) as f:
             self.cli.do_update("")
             self.assertEqual("** class name missing **\n", f.getvalue())
 
-    def test_do_create_valid(self):
-        """Test create command with valid input"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.cli.do_create("BaseModel")
-            self.assertTrue(len(f.getvalue().strip()) > 0)
-
-    def test_do_show_invalid_class(self):
-        """Test show command with invalid class"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.cli.do_show("NotAClass 1234-1234-1234")
-            self.assertEqual("** class doesn't exist **\n", f.getvalue())
-
-    def test_do_destroy_invalid_class(self):
-        """Test destroy command with invalid class"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.cli.do_destroy("NotAClass 1234-1234-1234")
-            self.assertEqual("** class doesn't exist **\n", f.getvalue())
-
-    def test_do_update_invalid_class(self):
-        """Test update command with invalid class"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.cli.do_update("NotAClass 1234-1234-1234 attr value")
-            self.assertEqual("** class doesn't exist **\n", f.getvalue())
-
-    def test_counter(self):
-        """Test counter method"""
-        count = self.cli.counter("BaseModel")
-        self.assertEqual(count, len([obj for obj in storage.all().values() if type(obj).__name__ == "BaseModel"]))
-
-    def test_default_invalid_command(self):
-        """Test default method with invalid command"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.cli.default("NotAClass.not_a_method()")
-            self.assertEqual("", f.getvalue())
-
-
 if __name__ == '__main__':
-    """Main entry point for script"""
     unittest.main()
